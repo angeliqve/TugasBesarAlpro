6c68af2f75c90691c1e40dc60b5f3707629f15f5
Hudayullah A.K <hudayullah12@gmail.com>
2025-06-11 14:28:11 +0700

    Create LiteO.cpp

---
+++ LiteO.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <stdlib.h>
+#include <time.h>
+#include <windows.h>
+using namespace std;
+
+void titleCard();
+void printBoard();
+
+
+int main()
+{
+
+    titleCard();
+
+    char mainBoard [21][51] = {
+        
+    };
+
+
+
+    return 0;
+}
+
+void titleCard()
+{
+    cout << R"(
+ _     _ _              _____ 
+| |   (_) |            |  _  |
+| |    _| |_ ___ ______| | | |
+| |   | | __/ _ \______| | | |
+| |___| | ||  __/      \ \_/ /
+\_____/_|\__\___|       \___/ 
+                
+
+
+)";
+
+    // menggunakan https://patorjk.com/software/taag/#p=display&f=Sub-Zero&t=Lite-O dengan font DOOM
+}
+
+void printBoard(){
+    
+}
