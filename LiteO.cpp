73f8cbab82e88aeb5543fa932ad9361266b75218
Hudayullah A.K <hudayullah12@gmail.com>
2025-06-11 14:49:36 +0700

    Update LiteO.cpp

--- LiteO.cpp
+++ LiteO.cpp
@@ -7,27 +7,33 @@
 using namespace std;
 
 /*
+Anggota Kelompok:
+Angelique Gabriel
+Bintang Rahmadi
+Hudayullah Ayasya Khoirizal
+
 Credit:
 Main Program:
 Void TitleCard: Hudayulah
 void printBoard: Hudayullah
 */
 
-
 void titleCard();
-void printBoard();
-
+void printBoard(char board[21][51]);
 
 int main()
 {
 
     titleCard();
 
-    char mainBoard [21][51] = {
-        
-    };
+    char mainBoard[21][51];
 
-
+    for (int i = 0; i < 21; i++)
+    {
+        for (int j = 0; j < 51; j++)
+        {
+        }
+    }
 
     return 0;
 }
@@ -49,6 +55,6 @@ void titleCard()
     // menggunakan https://patorjk.com/software/taag/#p=display&f=Sub-Zero&t=Lite-O dengan font DOOM
 }
 
-void printBoard(){
-    
+void printBoard(char board[21][51])
+{
 }
