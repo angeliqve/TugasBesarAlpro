14e6f0fcd824e04f176992aab54bce052d39704e
Hudayullah A.K <hudayullah12@gmail.com>
2025-06-11 15:15:52 +0700

    Update LiteO.cpp

--- LiteO.cpp
+++ LiteO.cpp
@@ -15,25 +15,37 @@ Hudayullah Ayasya Khoirizal
 Credit:
 Main Program:
 Void TitleCard: Hudayulah
+void topBottom: Hudayullah
 void printBoard: Hudayullah
 */
 
 void titleCard();
-void printBoard(char board[21][51]);
+void topBottomBoard();
+void printBoard(string board[21][51]);
 
 int main()
 {
 
     titleCard();
 
-    char mainBoard[21][51];
+    string mainBoard[21][51];
+
+    /*
+        Di modul tugas, disuruh untuk tabel berukuran 20, dan 50.
+        Tapi di contoh tabel terhitung panjang 21, dan lebar 51.
+        Jadi Diasumsikan bahwa tabel dihitung dimulai dari 0.
+        jadi panjangnya adalah 0-20, dan lebarnya berukuran 0-50.
+    */
 
     for (int i = 0; i < 21; i++)
     {
         for (int j = 0; j < 51; j++)
         {
+            mainBoard[i][j] = " ";
         }
     }
+
+    printBoard(mainBoard);
 
     return 0;
 }
@@ -55,6 +67,28 @@ void titleCard()
     // menggunakan https://patorjk.com/software/taag/#p=display&f=Sub-Zero&t=Lite-O dengan font DOOM
 }
 
-void printBoard(char board[21][51])
+void topBottomBoard()
 {
+    cout << "|";
+    for (int i = 0; i < 51; i++)
+        cout << "-";
+    cout << "|" << endl;
+}
+
+void printBoard(string board[21][51])
+{
+    topBottomBoard();
+
+    for (int i = 0; i < 21; i++)
+    {
+        cout << "|";
+        for (int j = 0; j < 51; j++)
+        {
+            cout << board[i][j];
+        }
+        cout << "|" << endl;
+    }
+
+    topBottomBoard();
+    cout << endl;
 }
