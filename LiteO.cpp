25b7787044da5aad554833ce3265a314124188b3
Hudayullah A.K <hudayullah12@gmail.com>
2025-06-15 22:57:05 +0700

    Update LiteO.cpp

--- LiteO.cpp
+++ LiteO.cpp
@@ -5,6 +5,7 @@
 #include <time.h>
 #include <windows.h>
 #include <vector>
+#include <iomanip>
 using namespace std;
 
 /*
@@ -12,6 +13,25 @@ Anggota Kelompok:
 1. Angelique Gabriel
 2. Bintang Rahmadi
 3. Hudayullah Ayasya Khoirizal
+
+credit:
+struct coordinates: Huda
+struct Hadiah     :
+vector listHadiah :
+void titlecard    : Huda
+void PrintBoard   : Huda
+void topBottom    : H
+void menu         : 
+void wait         : 
+void importtgerak : 
+void animation    : Huda
+void editTgerak   : Huda
+void bacaHadiah   :
+void simpanHadiah :
+void tambahHadiah :
+
+Main Program      : Angel, Bintang,& Huda
+
 */
 
 typedef struct
@@ -20,17 +40,19 @@ typedef struct
     string y;
 } coordinates;
 
-struct Hadiah{
+struct Hadiah
+{
     int x, y;
     string nama;
     int skor;
 };
 
+vector<Hadiah> listHadiah;
+
 void titleCard();
-void topBottomBoard(string board[21][51]);
+void topBottomBoard();
 void printBoard(string board[21][51], int score);
 void menu(char &choice);
-void SwitchChoice(char &choice);
 void wait(float x);
 void importtgerak(vector<coordinates> &tampungan);
 void animation(vector<coordinates> &tampungan, string mainBoard[21][51], int score);
@@ -42,65 +64,47 @@ void tambahHadiah();
 int main()
 {
     bool infLoop = true;
-
     string mainBoard[21][51];
-    /*
-        Di modul tugas, disuruh untuk tabel berukuran 20, dan 50.
-        Tapi di contoh tabel terhitung panjang 21, dan lebar 51.
-        Jadi Diasumsikan bahwa tabel dihitung dimulai dari 0.
-        jadi panjangnya adalah 0-20, dan lebarnya berukuran 0-50.
-    */
-    string filename = "thadiah.txt";
-    vector<string> lines;
-    string line, pagar = "## ## ## ##";
-    int idxPagar = -1;
-
-    ifstream infile(filename);
-    if (infile.is_open()) {
-        while (getline(infile, line)) {
-            lines.push_back(line);
-            if (line == pagar) idxPagar = lines.size() - 1;
-        }
-        infile.close();
-    } else {
-        cout << "Tidak bisa membuka file untuk dibaca.\n";
-        return 1;
-    }
-
     int score = 0;
-    coordinates movement[100];
     char choice;
 
     for (int i = 0; i < 21; i++)
-    {
         for (int j = 0; j < 51; j++)
-        {
             mainBoard[i][j] = " ";
-        }
-    }
+
+    bacaHadiah(); // load hadiah on startup
 
     while (infLoop)
     {
         vector<coordinates> tampungan;
-
         importtgerak(tampungan);
 
         titleCard();
-
         printBoard(mainBoard, score);
-        menu(choice);
 
-        if (choice == '3')
+        do
         {
-            animation(tampungan, mainBoard, score);
-        }
-        else if (choice == '4')
-        {
-            return 0;
-        }
+            menu(choice);
+            switch (choice)
+            {
+            case '1':
+                tambahHadiah();
+                break;
+            case '2':
+                editTgerak();
+                break;
+            case '3':
+                animation(tampungan, mainBoard, score);
+                break;
+            case '4':
+                cout << "Exiting Program..." << endl;
+                return 0;
+                break;
+            default:
+                cout << "\nERROR! Input tidak Valid! Coba Lagi:\n\n";
+            }
+        } while (choice != '1' && choice != '2' && choice != '3' && choice != '4');
     }
-
-    return 0;
 }
 
 void titleCard()
@@ -112,11 +116,9 @@ void titleCard()
 | |   | | __/ _ \______| | | |
 | |___| | ||  __/      \ \_/ /
 \_____/_|\__\___|       \___/ 
-
-
+                              
 )";
-
-    // menggunakan https://patorjk.com/software/taag/#p=display&f=Sub-Zero&t=Lite-O dengan font DOOM
+    // Generated using https://patorjk.com/software/taag/#p=display&f=Sub-Zero&t=Lite-O with the font Doom
 }
 
 void topBottomBoard()
@@ -134,7 +136,6 @@ void printBoard(string board[21][51], int score)
     for (int i = 0; i < 21; i++)
     {
         cout << "|";
-
         for (int j = 0; j < 51; j++)
         {
             cout << board[i][j];
@@ -149,7 +150,6 @@ void printBoard(string board[21][51], int score)
 void menu(char &choice)
 {
     cout << R"(
-
 Menu: 
 1. Tambah Hadiah
 2. Tambah gerak O
@@ -158,34 +158,11 @@ Menu:
 Masukkan Menu: )";
 
     cin >> choice;
-    SwitchChoice(choice);
-}
-
-void SwitchChoice(char &choice)
-{
-    switch (choice)
-    {
-    case '1':
-        cout << "ADD GIFT";
-        break;
-    case '2':
-        cout << "ADD MOVEMENT";
-        break;
-    case '3':
-        break;
-    case '4':
-        cout << "Exiting Program...";
-        break;
-    default:
-        cout << "ERROR! input tidak valid, coba lagi!";
-        menu(choice);
-    }
 }
 
 void wait(float x)
 {
-    time_t start;
-    time_t current;
+    time_t start, current;
     time(&start);
     do
         time(&current);
@@ -195,7 +172,6 @@ void wait(float x)
 void importtgerak(vector<coordinates> &tampungan)
 {
     coordinates data;
-    int jumlahData = 0;
 
     fstream gerak("tgerak.txt", ios::in);
     if (!gerak)
@@ -203,10 +179,11 @@ void importtgerak(vector<coordinates> &tampungan)
         cout << "Gagal membuka file.\n";
         return;
     }
+
     gerak >> data.x >> data.y;
     if (data.x == "##")
     {
-        cout << "arsip kosong" << endl;
+        cout << "Arsip kosong." << endl;
     }
     else
     {
@@ -214,7 +191,6 @@ void importtgerak(vector<coordinates> &tampungan)
         {
             tampungan.push_back(data);
             gerak >> data.x >> data.y;
-
         } while (data.x != "##");
 
         gerak.close();
@@ -225,19 +201,14 @@ void animation(vector<coordinates> &tampungan, string mainBoard[21][51], int sco
 {
     for (const auto &coord : tampungan)
     {
-        // cout << "X: " << coord.x << "\tY: " << coord.y << endl;
         int x = stoi(coord.x);
         int y = stoi(coord.y);
 
         titleCard();
 
         for (int i = 0; i < 21; i++)
-        {
             for (int j = 0; j < 51; j++)
-            {
                 mainBoard[i][j] = " ";
-            }
-        }
 
         mainBoard[y][x] = "O";
         printBoard(mainBoard, score);
@@ -270,63 +241,128 @@ void editTgerak()
 
     while (getline(gerak, baris))
     {
+        if (baris == "## ##")
+        {
+            break;
+        }
         isidata.push_back(baris);
     }
+    gerak.close();
+
+    isidata.push_back(stringX + " " + stringY);
+
+    fstream putUpdated("tgerak.txt", ios::out);
+    for (const string &line : isidata)
+    {
+        putUpdated << line << endl;
+    }
+
+    putUpdated << "## ##\n";
+    putUpdated.close();
+
+    cout << "\nPergerakkan Koordinat sudah diperbarui.\n";
 }
 
-vector<Hadiah> listHadiah;
-
-void bacaHadiah(){
+void bacaHadiah()
+{
     ifstream file("thadiah.txt");
 
-    if(!file){
+    if (!file)
+    {
         cout << "File tidak ditemukan atau gagal dibuka.\n";
         return;
     }
 
     Hadiah h;
-    while(file >> h.x >> h.y >> h.nama >> h.skor){
+    while (file >> h.x >> h.y >> h.nama >> h.skor)
+    {
         listHadiah.push_back(h);
     }
     file.close();
 }
 
-void simpanHadiah(Hadiah &h){
-    ofstream file("thadiah.txt", ios::app); // append mode
-    if (!file){
+void simpanHadiah(Hadiah &h)
+{
+    ofstream file("thadiah.txt", ios::app);
+    if (!file)
+    {
         cout << "Gagal menyimpan ke file.\n";
         return;
     }
-    file << h.x << " " << h.y << " " << h.nama << " " << h.skor << endl;
 
-    file << "## ## ## ##" << endl;
+    file << h.x << " " << h.y << " " << h.nama << " " << h.skor << endl;
     file.close();
 }
 
-void tambahHadiah(){
-    cout << "Isi hadiah saat ini:" << endl;
-    cout << "---------------------------" << endl;
-    cout << "|x    |y    |nama  |skor  |" << endl;
-    cout << "---------------------------" << endl;
-    for (auto &h : listHadiah) {
-        cout << "|" << h.x << "    |" << h.y << "    |" << h.nama << "  |" << h.skor << "  |" << endl;
+void tambahHadiah()
+{
+    cout << "Isi hadiah saat ini:\n";
+    cout << "-------------------------------\n";
+    cout << left << setw(6) << "|x" << setw(6) << "|y" << setw(10) << "|nama" << setw(6) << "|skor|\n";
+    cout << "-------------------------------\n";
+
+    for (auto &h : listHadiah)
+    {
+        cout << "|" << setw(5) << h.x
+             << "|" << setw(5) << h.y
+             << "|" << setw(9) << h.nama
+             << "|" << setw(5) << h.skor << "|\n";
     }
-    cout << "---------------------------" << endl;
+
+    cout << "-------------------------------\n";
 
     char lanjut;
-    cout << "ingin mengisi: (Y/T) ";
+    cout << "Ingin mengisi? (Y/T): ";
     cin >> lanjut;
-    if (lanjut == 'Y' || lanjut == 'y') {
+
+    if (lanjut == 'Y' || lanjut == 'y')
+    {
         Hadiah h;
-        cout << "x: "; 
+        cout << "x: ";
         cin >> h.x;
-        cout << "y: "; 
+        cout << "y: ";
         cin >> h.y;
-        cout << "nama: "; 
+        cout << "nama: ";
         cin >> h.nama;
-        cout << "skor: "; 
+        cout << "skor: ";
         cin >> h.skor;
+
         listHadiah.push_back(h);
         simpanHadiah(h);
     }
 }
+/*
+BackUp incase anything goes wrong:
+
+tgerak:
+0 0
+0 1
+0 2
+0 3
+0 4
+0 5
+1 5
+2 5
+3 5
+4 5
+5 5
+6 5
+7 5
+8 5
+9 5
+10 5
+10 6
+10 7
+## ##
+
+thadiah:
+5 5 aa 5
+10 5 ab 10
+25 10 ac 5
+15 15 ad 10
+30 15 ae 5
+40 17 af 20
+## ## ## ##
+
+ */
+
